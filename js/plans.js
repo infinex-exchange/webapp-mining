4aeda922adc403be50e9b9b49347d52872b32198
vayamos-exchange <admin@vayamos.cc>
2022-09-11 13:39:31 +0200

    jumbo bg

--- js/plans.js
+++ js/plans.js
@@ -59,7 +59,13 @@ function renderPlan(planid, data) {
 		            </div>
 		            <div class="col-12 col-lg-6">
                         <div class="row">
-                            <div class="col-12 pt-3 pt-lg-0 pb-2">
+                            <div class="col-12 pt-3 pt-lg-0">
+                                <div class="row">
+                                    <div class="col-4">
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-12 pt-5 pt-lg-3 pb-2">
                                 <input type="range" class="form-range" min="${data.min_ord_units}" max="${data.avbl_units}" step="1" value="${data.min_ord_units}" oninput="recalcPlan(${planid})">
                             </div>
                             <div class="col-5 my-auto text-center">
