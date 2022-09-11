f6865d7760f65b3452877c5bc03bbc0a102426f8
vayamos-exchange <admin@vayamos.cc>
2022-09-11 11:11:24 +0200

    Default stroke

--- js/plans.js
+++ js/plans.js
@@ -73,7 +73,6 @@ function renderPlan(planid, data) {
             }
         },
         stroke: {
-            width: [5,5,4],
             curve: 'straight'
         },
         labels: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16],
