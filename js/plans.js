1c078b2b761168cb2515f55a9b8bc4c7fb9bab36
vayamos-exchange <admin@vayamos.cc>
2022-09-11 11:23:13 +0200

    Remove example data

--- js/plans.js
+++ js/plans.js
@@ -51,20 +51,7 @@ function renderPlan(planid, data) {
         labels.push(i);
     
     var options = {
-        series: [
-            {
-                name: 'Peter',
-                data: [5, 5, 10, 8, 7, 5, 4, null, null, null, 10, 10, 7, 8, 6, 9, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1]
-            },
-            {
-                name: 'Johnny',
-                data: [10, 15, null, 12, null, 10, 12, 15, null, null, 12, null, 14, null, null, null]
-            },
-            {
-                name: 'David',
-                data: [null, null, null, null, 3, 4, 1, 3, 4,  6,  7,  9, 5, null, null, null]
-            }
-        ],
+        series: [],
         chart: {
             height: 350,
             type: 'line',
@@ -88,8 +75,8 @@ function renderPlan(planid, data) {
                 }
             }
         },
-        dataLabels: {
-            enabled: false
+        noData: {
+            text: 'Loading...'
         }
     };
 
