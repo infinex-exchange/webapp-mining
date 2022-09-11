0f21b70b1179cee1bcffed6cee9a0a61315195fc
vayamos-exchange <admin@vayamos.cc>
2022-09-11 10:49:02 +0200

    Columns

--- js/plans.js
+++ js/plans.js
@@ -24,10 +24,14 @@ function renderPlan(planid, data) {
                             </div>
                         </div>
 		            </div>
-		            <div class="col-12">
-                        <input type="range" class="form-range" min="0" max="0" step="1" value="0">
+		            <div class="col-12 col-lg-6">
+                        <div class="row">
+                            <div class="col-12">
+                                <input type="range" class="form-range" min="0" max="0" step="1" value="0">
+                            </div>
+                        </div>
 		            </div>
-		            <div class="col-4">
+		            <div class="col-12 col-lg-6">
 		            </div>
 	            </div>
 	        </div>
