fccd3fc8716d7e9845627404e03af153f16b8521
vayamos-exchange <admin@vayamos.cc>
2022-09-11 10:51:27 +0200

    Investment forecast

--- js/plans.js
+++ js/plans.js
@@ -32,6 +32,13 @@ function renderPlan(planid, data) {
                         </div>
 		            </div>
 		            <div class="col-12 col-lg-6">
+                        <div class="row">
+                            <div class="col-12 text-center">
+                                <h5 class="secondary">
+                                    Investment forecast
+                                </h5>
+                            </div>
+                        </div>
 		            </div>
 	            </div>
 	        </div>
