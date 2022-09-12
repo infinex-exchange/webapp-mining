1f2bcf7d4b8af71520849357fc15ee681d7b11a0
vayamos-exchange <admin@vayamos.cc>
2022-09-12 14:32:18 +0200

    Paddings

--- js/plans.js
+++ js/plans.js
@@ -197,7 +197,7 @@ function renderPlan(planid, data) {
                                     
                                 </div>
                             </div>
-                            <div class="col-12 pt-5 pt-lg-3 pb-2">
+                            <div class="col-12 pt-5 pb-2">
                                 <input type="range" class="form-range" min="${data.min_ord_units}" max="${data.avbl_units}" step="1" value="${data.min_ord_units}" oninput="recalcPlan(${planid})">
                             </div>
                             <div class="col-5 my-auto text-center">
