6787dc3bebd32836970e13b4a51026356d0ee21a
vayamos-exchange <admin@vayamos.cc>
2022-09-12 14:27:02 +0200

    Add h-100

--- js/plans.js
+++ js/plans.js
@@ -180,7 +180,7 @@ function renderPlan(planid, data) {
                                     </div>
                                     
                                     <div class="col-4">
-                                        <div class="row">
+                                        <div class="row h-100">
                                             <div class="col-12">
                                                 <h5 class="secondary">
                                                     Total revenue
