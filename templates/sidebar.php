f1c57abeff96106b1550a887946567d947361db3
vayamos-exchange <admin@vayamos.cc>
2022-09-13 10:27:22 +0200

    Hide br in sidebar

--- templates/sidebar.php
+++ templates/sidebar.php
@@ -1,4 +1,4 @@
-<div class="d-none d-lg-block col-3 p-0 ui-card ui-column">
+<div class="sidebar d-none d-lg-block col-3 p-0 ui-card ui-column">
         
     <div class="row p-2">
         <h2>Mining</h2>
