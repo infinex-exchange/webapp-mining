f8f3293b914cf5a35592354519d5c618dabd991c
vayamos-exchange <admin@vayamos.cc>
2022-09-13 14:30:37 +0200

    Payouts skeleton

---
+++ js/payouts.js
@@ -0,0 +1,63 @@
+function renderPayoutItem(data) {     
+    var cTime = new Date(data.create_time * 1000).toLocaleString();
+    
+    return `
+        Payout
+    `;
+}
+
+$(document).ready(function() {
+    window.renderingStagesTarget = 1;
+});
+
+$(document).on('authChecked', function() {
+    if(window.logedIn) {
+        window.payoutsAS = new AjaxScroll(
+            $('#payouts-data'),
+            $('#payouts-data-preloader'),
+            {
+                api_key: window.apiKey
+            },
+            function() {
+                this.data.offset = this.offset;
+                var thisAS = this;
+                    
+                $.ajax({
+                    url: config.apiUrl + '/mining/payouts',
+                    type: 'POST',
+                    data: JSON.stringify(thisAS.data),
+                    contentType: "application/json",
+                    dataType: "json",
+                })
+                .retry(config.retry)
+                .done(function (data) {
+                    if(data.success) {
+                        $.each(data.payouts, function() {
+                            thisAS.append(renderTxHistoryItem(this));
+                        });
+                        
+                        thisAS.done();
+                    
+                        if(thisAS.offset == 0)
+                            $(document).trigger('renderingStage');
+                            
+                        if(data.transactions.length != 50)
+                            thisAS.noMoreData();
+                    }
+                    else {
+                        msgBoxRedirect(data.error);
+                        thisAS.done();
+                        thisAS.noMoreData();
+                    }
+                })
+                .fail(function (jqXHR, textStatus, errorThrown) {
+                    msgBoxNoConn(true);
+                    thisAS.done();
+                    thisAS.noMoreData();
+                });
+            },
+            true,
+            true
+        );
+    }
+});
---
+++ payouts.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <?php include('../../templates/head.php'); ?>
+        <link rel="stylesheet" href="/mining/css/styles.css?<?php echo filemtime(__DIR__.'/css/styles.css'); ?>">
+        <title>Payouts | Vayamos Mining</title>
+    </head>
+    <body>
+    
+        <!-- Preloader -->
+        <?php include('../../templates/preloader.html'); ?>
+        
+        <!-- Navbar -->
+        <?php include('../../templates/navbar.php'); ?>
+        <?php include(__DIR__.'/templates/navbar.php'); ?>
+        
+        <!-- Root container -->
+        <div id="root" class="container-fluid container-1500 container-rest p-0 user-only">
+        <div class="row m-0 h-rest">
+        
+        <!-- Main column -->
+        <div class="col-12 p-0 ui-card ui-column">
+            
+            <div class="row p-2">
+                <h3>Payouts</h3>
+            </div>
+            
+            <div class="row p-2 secondary d-none d-lg-flex">
+                <div style="width: 25%">
+                    <h5>Date</h5>
+                </div>
+                <div style="width: 25%">
+                    <h5>Asset</h5>
+                </div>
+                <div class="text-end" style="width: 25%">
+                    <h5>Amount</h5>
+                </div>
+                <div class="text-end" style="width: 25%">
+                    <h5>Power</h5>
+                </div>
+            </div>
+            
+            <div id="payouts-data">
+            </div>
+        
+        <!-- / Main column -->
+        </div>
+            
+        <!-- / Root container -->    
+        </div>
+        </div>
+        
+        <script src="/mining/js/payouts.js?<?php echo filemtime(__DIR__.'/js/payouts.js'); ?>"></script>
+        
+        <?php include('../../templates/modals.php'); ?>
+        <?php include(__DIR__.'/templates/mobile_nav.php'); ?>
+    
+    </body>
+</html>
