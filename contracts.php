973b5c24e42b09c56f1d49fa957ea206083a9157
vayamos-exchange <admin@vayamos.cc>
2022-09-13 12:19:13 +0200

    Import bignumber and apex

--- contracts.php
+++ contracts.php
@@ -2,6 +2,8 @@
 <html lang="en">
     <head>
         <?php include('../../templates/head.php'); ?>
+        <?php include('../../imports/bignumber.html'); ?>
+        <?php include('../../imports/apexcharts.html'); ?>
         <script src="/js/ajax_scroll.js?<?php echo filemtime(__DIR__.'/../../js/ajax_scroll.js'); ?>"></script>
         <link rel="stylesheet" href="/mining/css/styles.css?<?php echo filemtime(__DIR__.'/css/styles.css'); ?>">
         <title>My contracts | Vayamos Mining</title>
