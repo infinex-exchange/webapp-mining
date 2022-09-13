dfd75604ae08c76e00984c67a3b66de286ae32ed
vayamos-exchange <admin@vayamos.cc>
2022-09-13 09:57:33 +0200

    Mining mobile nav

--- index.php
+++ index.php
@@ -14,7 +14,7 @@
         
         <!-- Navbar -->
         <?php include('../../templates/navbar.php'); ?>
-        <?php include(__DIR__.'/templates/navbar.php'); ?>
+        <?php include(__DIR__.'/templates/navbar.html'); ?>
         
         <!-- Root container -->
         <div id="root" class="container-fluid container-1500 h-rest pt-2 p-0">
@@ -102,7 +102,7 @@
         
         <!-- Footer -->
         <?php include('../../templates/footer.html'); ?>
-        <?php include('../../templates/vanilla_mobile_nav.php'); ?>
+        <?php include(__DIR__.'/templates/mobile_nav.html'); ?>
     
     </body>
 </html>
