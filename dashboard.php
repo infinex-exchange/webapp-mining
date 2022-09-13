204f49ed42a0774afd3a3fd387823721304b539e
vayamos-exchange <admin@vayamos.cc>
2022-09-13 10:31:39 +0200

    Include css

--- dashboard.php
+++ dashboard.php
@@ -2,6 +2,7 @@
 <html lang="en">
     <head>
         <?php include('../../templates/head.php'); ?>
+        <link rel="stylesheet" href="/mining/css/styles.css?<?php echo filemtime(__DIR__.'/css/styles.css'); ?>">
         <title>Dashboard | Vayamos Mining</title>
     </head>
     <body>
