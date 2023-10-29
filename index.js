05d30d37b885bc0e9f211b7926eb766d2025ad17
infinex-exchange <admin@infinex.cc>
2023-10-29 12:56:16 +0100

    Debug

--- index.js
+++ index.js
@@ -303,6 +303,7 @@ function recalcPlan(planid) {
     let revenDetailed = ''; // All assets total revenue string <br> separated
     
     for(const i in dailyNative) {
+        console.log(dailyNative[i]);
         dailyDetailed += prettyBalance(dailyNative[i], data.assets[i].defaultPrec)
                       +  ' ' + data.assets[i].symbol + '<br>';
         revenDetailed += prettyBalance(dailyNative[i].times(days), data.assets[i].defaultPrec)
